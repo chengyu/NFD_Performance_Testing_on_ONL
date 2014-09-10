a1e7f50538c21cb8df500184175ba9eceb00d08d
jdehart1 <jdd@arl.wustl.edu>
2014-09-10 15:52:56 -0500

    add sleep so rtr can dump gmon.out with interference from other nfds

--- Variable_Length_Names/killAll.sh
+++ Variable_Length_Names/killAll.sh
@@ -24,6 +24,10 @@ do
   ssh ${!s} "killall nfd"
 done
 
+echo "sleep 10 to give nfd from clients and servers to dump gmon.out if they are. Then rtr can be the last"
+
+sleep 10
+
 echo "Kill nfd processes on Rtr"
 ssh ${!RTR_HOST} "killall nrd"
 ssh ${!RTR_HOST} "killall nfd"
