d70dc4c99b39e767d39e668f1d6bb40198cb74ec
jdehart1 <jdd@arl.wustl.edu>
2016-02-11 09:25:24 -0600

    cleanup and fixes and comments

--- Variable_Length_Names/configAll.sh
+++ Variable_Length_Names/configAll.sh
@@ -11,8 +11,18 @@ fi
 
 CWD=`pwd`
 
+echo ""
+echo "-----CONFIG SERVERS-----"
+echo ""
 ./configServers.sh ${PROTO}
+echo ""
+echo "-----CONFIG CLIENTS-----"
+echo ""
 ./configClients.sh ${PROTO}
 
-#echo "ssh ${!RTR_HOST} \"cd $CWD/rtr; ./configRtr $PROTO\""
-ssh ${!RTR_HOST} "cd $CWD/rtr; ./configRtr.sh $PROTO"
+echo ""
+echo "-----CONFIG ROUTER-----"
+echo ""
+##echo "ssh ${!RTR_HOST} \"cd $CWD/rtr; ./configRtr $PROTO\""
+#ssh ${!RTR_HOST} "cd $CWD; source PATH.env; cd rtr; ./configRtr.sh $PROTO"
+ssh ${!RTR_HOST} "cd $CWD; cd rtr; ./configRtr.sh $PROTO"
